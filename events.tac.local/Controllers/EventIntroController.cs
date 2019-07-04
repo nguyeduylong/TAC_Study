541e23b7975eeb425e408d17b19ee97616e34c35
smartosc_conglv <conglv.jp@smartosc.com>
2019-07-04 11:40:47 +0700

    TAC_Sl

---
+++ events.tac.local/Controllers/EventIntroController.cs
@@ -0,0 +1,43 @@
+﻿using events.tac.local.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Web.UI.WebControls;
+
+namespace events.tac.local.Controllers
+{
+    /// <summary>
+    /// EventIntroController
+    /// </summary>
+    public class EventIntroController : Controller
+    {
+        // GET: EventIntro
+        public ActionResult Index()
+        {
+            return View(CreateModel());
+        }
+        /// <summary>
+        /// CreateModel
+        /// </summary>
+        /// <returns></returns>
+        public static EventIntro CreateModel()
+        {
+            var item = RenderingContext.Current.ContextItem;
+            return new EventIntro()
+            {
+                ContentHeading = new HtmlString(FieldRenderer.Render(item, "ContentHeading")),
+                ContentBody = new HtmlString(FieldRenderer.Render(item, "ContentBody")),
+                EventImage = new HtmlString(FieldRenderer.Render(item,"EventImage", "mw=720")),
+                Highlights = new HtmlString(FieldRenderer.Render(item, "Highlights")),
+                ContentIntro = new HtmlString(FieldRenderer.Render(item, "ContentIntro")),
+                StartDate = new HtmlString(FieldRenderer.Render(item, "StartDate","Format=dd MMM, yyyy")),
+                Duration = new HtmlString(FieldRenderer.Render(item, "Duration")),
+                DifficultyLevel = new HtmlString(FieldRenderer.Render(item,"DifficultyLevel"))
+            };
+        }
+       
+    }
+}
---
+++ events.tac.local/Models/EventIntro.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace events.tac.local.Models
+{
+    /// <summary>
+    /// EventIntro Model
+    /// </summary>
+    public class EventIntro
+    {
+        /// <summary>
+        /// ConTentHeading Prop
+        /// </summary>
+        public HtmlString ContentHeading { get; set; }
+
+        /// <summary>
+        /// ContentIntro Prop
+        /// </summary>
+        public HtmlString ContentIntro { get; set; }
+
+        /// <summary>
+        /// ContentBody Prop
+        /// </summary>
+        public HtmlString ContentBody { get; set; }
+
+        /// <summary>
+        /// EventImage Prop
+        /// </summary>
+        public HtmlString EventImage { get; set; }
+
+        /// <summary>
+        /// Highlights Prop
+        /// </summary>
+        public HtmlString Highlights { get; set; }
+
+        /// <summary>
+        /// StarDate Prop
+        /// </summary>
+        public HtmlString StartDate { get; set; }
+
+        /// <summary>
+        /// Duration Prop
+        /// </summary>
+        public HtmlString Duration { get; set; }
+
+        /// <summary>
+        /// DifficultyLevel Prop
+        /// </summary>
+        public HtmlString DifficultyLevel { get; set; }
+
+
+    }
+}
